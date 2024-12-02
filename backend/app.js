05f164ff904cc38ab9349d682b8904d7778b92db
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-12-02 23:07:06 +0100

    create server.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,31 @@
 const express = require('express');
 const app = express();
+
+// add middleware
+// next() method comes from middleware, it allows the middleware passes to next middleware. 
+// for example here the app Express contains four elements of middleware. 
+
+// The first one
+app.use((req, res, next) => {
+    console.log('middleware request is got');
+    next();
+})
+
+// The second one
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+// The third one
+app.use((req, res) => {
+    res.json( {message : 'We got your request!'})
+    next();
+});
+
+//  The fourth one, is last element of the middleware save
+app.use((req, res) => {
+    console.log('response is send with success!')
+
+})
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,64 @@
-console.log('Server js')
+console.log('This is my first server js!')
 
 // create un server Node 
 // import le package http, for doing this use require key word
 const http = require('http');
+// after creating a file app.js import it in the file server.js
+const app = require('./app')
 
-const server = http.createServer((req, res) => {
-    res.end('This is the response of server')
-})
+/* const server = http.createServer((req, res) => {
+   res.end('This is the response of server')
+}) */
 
-server.listen(process.env.PORT || 3000);
+// Now we have to develop our server.js
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+            default:
+                throw error;
+        }
+}
+
 // On the postman, check it, use the request GET => http://localhost:3000
 // coding server web on Node is possible but it is long and heavy
 // So it is better to use framework ExpressJS which simplifies the tasks and we can deploy our APIs fast
+app.set('port', process.env.PORT || 3000);
 
+const server = http.createServer(app);
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind)
+})
 
+server.listen(process.env.PORT || 3000);
